1c33f85177bbad6ff1f4f8779414a4459377c2df
Stangil <stangil44@gmail.com>
2019-01-30 13:02:15 -0500

    Put together basic admin layout

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
+using SimpleBlog.Infrastructure;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         public ActionResult Index()
         {
-            return Content("USERS");
+            return View();
         }
     }
 }
